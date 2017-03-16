c52cbb9287313e1b23367de8a7f977b61a726ce6
Thomas Steinacher <tom@eggdrop.ch>
2017-03-16 18:26:16 +0100

    rollback_on_error decorator

--- flask_restutils/helpers.py
+++ flask_restutils/helpers.py
@@ -1,4 +1,5 @@
 from flask import current_app, request
+from functools import wraps
 from werkzeug.exceptions import BadRequest
 
 def get_db(app=None):
@@ -10,3 +11,19 @@ def request_json():
     if not request.is_json:
         raise BadRequest({'errors': ['Invalid request: application/json expected.']})
     return request.get_json()
+
+def rollback_on_error(session):
+    """
+    Function that returns a decorator that rolls back the given session if an
+    exception occurrs.
+    """
+    def decorator(func):
+        @wraps(func)
+        def wrapped(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except:
+                session.rollback()
+                raise
+        return wrapped
+    return decorator
